c61a8c92924b30f2f08061bb96deed0b0b16322d
Audrey Carlsen <acarlsen@seattletimes.com>
2015-07-21 16:29:16 -0700

    loading data

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,13 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+var $ = require("jquery");
+var ich = require("icanhaz");
+var questionTemplate = require("./_questionTemplate.html");
+
+
+console.log(quizData)
+
+ich.addTemplate("questionTemplate", questionTemplate);
+console.log(quizData[1])
+$(".question-box").html(ich.questionTemplate(quizData[1]));
